936aba7f420de6933d32c7718d0f64def29b8753
Ubeydullah Yasir Yurtseven <yasir2001.uyy@gmail.com>
2025-12-27 01:35:36 +0300

    first commit

---
+++ src/js/02-form.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('.feedback-form');
+const storage = 'feedback-form-state';
+
+const savedData = JSON.parse(localStorage.getItem(storage));
+
+if (savedData) {
+  form.elements.email.value = savedData.email ?? '';
+  form.elements.message.value = savedData.message ?? '';
+}
+
+form.addEventListener('input', () => {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+
+  localStorage.setItem(storage, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  console.log(formData);
+
+  localStorage.removeItem(storage);
+  form.reset();
+});
